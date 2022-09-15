c49ba245612b084b877fb57dcf8db788ebd40300
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-09-15 20:00:18 +0300

    emoji

--- src/components/MobileFooter.jsx
+++ src/components/MobileFooter.jsx
@@ -4,7 +4,7 @@ import "./css/footer.css"
 function MobileFooter() {
   return (
     <div className="foot-container">
-      <p>LMG is wholly owned by NGENI  -  leading Web3 Engineering Group.</p>
+      <p>🔥🔥LMG is wholly owned by NGENI  -  leading Web3 Engineering Group.</p>
     </div>
   )
 }
