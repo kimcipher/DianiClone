425ca3e76edfd54a87255603a897c92dbaf0356f
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-09-15 18:58:10 +0300

    added icons

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import "./css/home.css";
 import ReactWhatsapp from "react-whatsapp";
+import Activity from "./assets/icons/Activity.png"
+import Beer from "./assets/icons/Beer.png"
+import bike from "./assets/icons/bike.png"
+import ForeignExchange from "./assets/icons/ForeignExchange.png"
+import Reservation from "./assets/icons/Reservation.png"
+import Transport from "./assets/icons/Transport.png"
 
 function Home() {
   const data = [
@@ -9,36 +15,48 @@ function Home() {
       number: "+254728000107",
       message: "Interested in Transportation",
       className: "whatsapp",
+      imahe:Transport,
+      desc:"image desc",
     },
     {
       name: "Foreign Exchange",
       number: "+254728000107",
       message: "Interested in Foreign Exchange",
       className: "whatsapp",
+      imahe:ForeignExchange,
+      desc:"image desc",
     },
     {
       name: "Drinks",
       number: "+254728000107",
       message: "Interested in Drinks",
       className: "whatsapp",
+      imahe:Beer,
+      desc:"image desc",
     },
     {
       name: "Activity Guide",
       number: "+254728000107",
       message: "Interested in Activity Guide",
       className: "whatsapp",
+      imahe:Activity,
+      desc:"image desc",
     },
     {
       name: "Errands",
       number: "+254728000107",
       message: "Interested in running Errands",
       className: "whatsapp",
+      imahe:bike,
+      desc:"image desc",
     },
     {
       name: "Health",
       number: "+254728000107",
       message: "Interested in Health",
       className: "whatsapp",
+      imahe:Reservation,
+      desc:"image desc",
     },
   ];
   return (
@@ -56,7 +74,7 @@ function Home() {
             message={item.message}
             className={item.className}
           >
-            <h4 className="Tab-item">{item.name}</h4>
+            <h4 className="Tab-item"> <img src={item.imahe} alt={item.desc}/> {item.name}</h4>
           </ReactWhatsapp>
         ))}
       </div>
