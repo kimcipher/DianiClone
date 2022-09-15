4b81c56f0b97e0770c18555eb78cdf98be17086d
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-09-15 20:05:41 +0300

    typo

--- src/components/MobileFooter.jsx
+++ src/components/MobileFooter.jsx
@@ -4,7 +4,7 @@ import "./css/footer.css"
 function MobileFooter() {
   return (
     <div className="foot-container">
-      <p> LMG is wholly owned by NGENI🔥  -  leading Web3 Engineering Group.</p>
+      <p> LMG is wholly owned by NGENI🔥  -  Leading Web3 Engineering Group.</p>
     </div>
   )
 }
