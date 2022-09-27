f168b917b9c08ebdfae8a2d1b443698eb4a12e43
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-09-27 13:59:38 +0300

    rm service worker registration

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import * as serviceWorker from "./registerServiceWorker"
+// import * as serviceWorker from "./registerServiceWorker"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -18,4 +18,4 @@ root.render(
 // // to log results (for example: reportWebVitals(console.log))
 // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
-serviceWorker.register();
+// serviceWorker.register();
