f3225e003391b8e84c9e26301e2e3c1ac0bd244d
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-10-02 16:40:19 +0300

    update footer

--- src/components/MobileFooter.jsx
+++ src/components/MobileFooter.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import "./css/footer.css";
-import Nai from "./assets/nAIROBI-ART.png"
-
+import Nai from "./assets/nAIROBI-ART.png";
 
 function MobileFooter() {
   const currentDate = new Date().getFullYear();
   return (
     <div className="foot-container">
-      <p><img src={Nai} alt="src alt" width="250px"/></p>
+      <p>
+        <img src={Nai} alt="src alt" width="250px" />
+      </p>
       <p>
         <a href="/how-it-works"> How it works</a>{" "}
       </p>
-      <p>
-      Powered by LaLo Communities.  A Subsidiary of NGENI IO LTD. 
-      </p>
+      <p>Powered by LaLo Communities.</p>
       <p>&copy; {currentDate} DianiApp .</p>
     </div>
   );
