b13234df41824d0783f0d2dd28a0afad4491b5df
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-10-04 16:25:12 +0300

    alignment update

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { mpesa, crypto, credit, paypal, kes } from "./icons";
+import { mpesanew, crypto, credit, paypal, kes } from "./icons";
 import "./css/Payment.css";
 const Payment = () => {
   return (
     <div className="Payment-container">
-      <h4 style={{fontSize:"0.8em"}}>Accepts</h4> &nbsp;&nbsp;&nbsp; {kes} {crypto} {credit} {paypal} {mpesa}
+      <h4 style={{fontSize:"0.8em"}}>Accepts</h4> &nbsp;&nbsp;&nbsp; {kes}&nbsp; {crypto} &nbsp; {credit} &nbsp; {paypal}&nbsp; {mpesanew}
     </div>
   );
 };
